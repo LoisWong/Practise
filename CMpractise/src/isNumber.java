b58d925ef3cd61a546ba7860c96429be5ec5b403
loiswong <loiswong90@gmail.com>
2015-07-28 01:17:55 +1000

    Regex

--- CMpractise/src/isNumber.java
+++ CMpractise/src/isNumber.java
@@ -12,43 +12,12 @@ public class isNumber {
 				System.out.println("False");
 		}while (!s.equalsIgnoreCase("exit"));
 	}
-    public static boolean isNumber(String old) {
-        String s = old.replaceAll("\\s+","");
-        int lenth = s.length();
-        char c;
-        boolean flag = false;
-        int dFlag = 0;
-        int eFlag = 0;
-        
-        for (int i = 0; i < lenth; i++){
-            c = s.charAt(i);
-            if (c>=48 && c<=57){
-                flag = true;
-            }else if (c == '.' && i >= 0){
-                dFlag++;
-                if (dFlag <= 1 && (i+1) < lenth){
-                    flag = true;
-                }else {
-                    flag = false;
-                    return false;
-                }
-            }else if (c == 'e' && i > 0){
-                eFlag++;
-                if (eFlag <= 1){
-                    flag = true;
-                }else {
-                    flag = false;
-                    return false;
-                }
-            }else if (c == '-'){
-                if (i == 0 && lenth >1){
-                    flag = true;
-                }else {
-                    flag = false;
-                    return false;
-                }
+    public static boolean isNumber(String s) {
+    	String n = s.replaceAll("\\s+","");
+        if (n.length() > 0){
+        	if (n.charAt(0) != 'e'){
+        		return n.matches("[-+]?[0-9]*(.[0-9]+)?(e[-+]?[0-9]+)?");
             }else return false;
-        }
-        return flag;
+        }else return false;
     }
 }
