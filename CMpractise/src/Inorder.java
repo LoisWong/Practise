fe32d23554bfcdf4f0d61dd0fa830b2f6adf156b
loiswong <loiswong90@gmail.com>
2015-08-01 23:25:23 +1000

    Binary Tree

--- CMpractise/src/Inorder.java
+++ CMpractise/src/Inorder.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Inorder {
     public List<Integer> inorderTraversal(TreeLinkNode root) {
-    	List<Integer> list = null;
-    	while (root != null){
-    		
-    		root = root.left;
+    	List<Integer> list = new ArrayList<Integer>();
+    	Stack<TreeLinkNode> stack = new Stack<TreeLinkNode>();
+    	TreeLinkNode temp = root;
+    	while (temp != null || !stack.empty()){
+    		while (temp != null){
+    			stack.push(temp);
+    			temp = temp.left;
+    		}
+    		temp = stack.pop();
+    		list.add(temp.val);
+    		temp = temp.right;
     	}
         return list;
     }
---
+++ CMpractise/src/Preorder.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Preorder {
+    public List<Integer> preorderTraversal(TreeLinkNode root) {
+    	List<Integer> nl = new ArrayList<Integer>();
+    	Stack<TreeLinkNode> stack = new Stack<TreeLinkNode>();
+    	stack.push(root);
+        if (root == null){
+            return nl;
+        }else{
+        	while (!stack.empty()){
+        		TreeLinkNode temp = stack.pop();
+        		if (temp != null){
+        			nl.add(temp.val);
+        			stack.push(temp.right);
+        			stack.push(temp.left);
+        		}
+        	}
+        	return nl;
+        }
+    }
+}
