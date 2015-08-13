aaad6491d4c66ff439bcd978a5586be335ea25aa
loiswong <loiswong90@gmail.com>
2015-08-14 01:57:44 +1000

    Metric Solved

--- CMpractise/src/MetricSearch.java
+++ CMpractise/src/MetricSearch.java
@@ -1,8 +1,8 @@
 
 public class MetricSearch {
 	public static void main(String[] args){
-		int[][] matrix = {{1},{3}};//{{1,3,5,7},{10,11,16,20},{23,30,34,50}};
-		int target = 3;
+		int[][] matrix = {{0}};//{{1,3,5,7},{10,11,16,20},{23,30,34,50}};
+		int target = 0;
 		if(searchMatrix(matrix, target)){
 			System.out.print("true");
 		}else System.out.print("false");
@@ -10,8 +10,8 @@ public class MetricSearch {
 	public static boolean searchMatrix(int[][] matrix, int target) {
         int up = 0;
         int left = 0;
-        int down = matrix.length-1;
-        int right = matrix[0].length-1;
+        int down = matrix.length;
+        int right = matrix[0].length;
         if (matrix[0][0] == target) return true;
         while (up != down-1 && up < down){
         	if (target >= matrix[(up+down)/2][0]){
