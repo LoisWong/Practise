ae2e8b7df8d4c47a47a1298c6f0de0d0d7aa16c8
loiswong <loiswong90@gmail.com>
2015-08-17 14:23:04 +1000

    Combine NOT solved

---
+++ CMpractise/src/Combine.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Combine {
+	 public List<List<Integer>> combine(int n, int k) {
+		 List<List<Integer>> row = new LinkedList<List<Integer>>();
+	        if (k > n) return row;
+	        else 
+	 }
+}
